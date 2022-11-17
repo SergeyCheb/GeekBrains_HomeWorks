8b702c76ae4def02175ef9279655b533fe1358a9
SergeyCheb <iphonecheb@gmail.com>
2022-11-17 23:14:07 +0300

    добавлена папка HomeWork6

---
+++ HomeWork6/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Insert a quantity of numbers you want to handle: ");
+int count = Convert.ToInt32(Console.ReadLine());
+if (count > 0)
+{
+    int tempVal = 0;
+    int biggerThanZeroCount = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        Console.Write("Insert the value: ");
+        tempVal = Convert.ToInt32(Console.ReadLine());
+        if (tempVal > 0)
+            ++biggerThanZeroCount;
+    }
+    Console.WriteLine($"The number of positive values is {biggerThanZeroCount}");
+} else {
+    Console.Write("The incorrect value is entered.");
+}
---
+++ HomeWork6/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+void AcrossCoordinates(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+        if (b1 == b2)
+            Console.WriteLine("These straight lines are coincide");
+        else
+            Console.WriteLine("These straight lines are parallel");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"These straight lines are across in the point ({Math.Round(x, 3)};{Math.Round(y, 3)})");
+    }
+}
+
+Console.Write("Insert b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+AcrossCoordinates(b1, k1, b2, k2);
